a3e884dd4ee0438a6020246364bacadae7002f2d
Ondrej Sika <ondrej@ondrejsika.com>
2022-02-22 11:48:14 +0100

    feat: Create app

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Head from "next/head";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+let emojis = ["👋", "🦄"];
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {emojis.map((el, i) => {
+        return (
+          <CopyToClipboard text={el}>
+            <button style={{ margin: "10px", fontSize: "2em" }}>{el}</button>
+          </CopyToClipboard>
+        );
+      })}
+    </>
+  );
+}
