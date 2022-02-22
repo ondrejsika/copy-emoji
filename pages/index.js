e20b74c5c71ab335b538cd9eda71835d56b3a968
Ondrej Sika <ondrej@ondrejsika.com>
2022-02-22 12:01:34 +0100

    feat: Add favicon

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,10 @@ export default function Home() {
       <Head>
         <title>My page title</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       {emojis.map((el, i) => {
         return (
