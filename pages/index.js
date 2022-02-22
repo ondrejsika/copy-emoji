ecbacb9e7d6830088b1f919ad4454f347978eb64
Ondrej Sika <ondrej@ondrejsika.com>
2022-02-22 12:02:08 +0100

    fix: Fix title

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>My page title</title>
+        <title>copy-emoji - em.oxs.cz</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
