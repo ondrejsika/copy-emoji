ae499355c5eb3fde9c7ad2d639d7bd1cca58a62b
Ondrej Sika <ondrej@ondrejsika.com>
2022-02-24 22:34:18 +0100

    feat: Add 😥

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "next/head";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
-let emojis = ["👋", "🦄", "⚠️", "💩"];
+let emojis = ["👋", "🦄", "⚠️", "💩", "😥"];
 
 export default function Home() {
   return (
